49f000478bc95a88ee68ce1b65730633315517de
MuzafferSenkal <muzaffersenkalpcmail.com>
2022-11-07 11:23:26 +0300

    Add project files.

---
+++ MyTurkcellCoreMVC101Project.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyTurkcellCoreMVC101Project.Web.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
